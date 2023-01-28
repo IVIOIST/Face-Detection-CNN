fb1da1fa3e3571a36f6f3987f7d3e14e4d70948c
Erik Sun <higgsboson16@protonmail.com>
2023-01-28 15:10:02 +1100

    Accpets multiple encodings, counts number of fails

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 import face_recognition
+import ctypes
+
 
 GPU = tf.config.list_physical_devices('GPU')
 try:
@@ -20,10 +22,16 @@ face_detector = load_model('VGG19_REV1.h5')
 my_face = face_recognition.load_image_file(os.path.join('face_data', 'known', 'face1.jpg'))
 my_face_encoding = face_recognition.face_encodings(my_face)[0]
 
+my_face2 = face_recognition.load_image_file(os.path.join('face_data', 'known', 'face2.jpg'))
+my_face_encoding2 = face_recognition.face_encodings(my_face2)[0]
+
+known_faces = [my_face_encoding, my_face_encoding2]
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+counter = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -45,9 +53,23 @@ while cap.isOpened():
             rgb_cropped_frame = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
             unknown_face_encoding = face_recognition.face_encodings(rgb_cropped_frame)
             result = face_recognition.compare_faces(my_face_encoding, unknown_face_encoding)
-            print(result)
+            flag = False
+            for known_face in known_faces:
+                results = face_recognition.compare_faces(known_face, unknown_face_encoding, tolerance=0.5)
+                if results[0] == True:  
+                    counter = 0
+                    flag = True
+                    print(flag)
+                    break
+            if flag == False:
+                counter += 1 
+                print(flag)
         except:
             pass
+        if counter == 3:
+            print('fail limit reached')
+            counter = 0
+            ctypes.windll.user32.LockWorkStation()
 
     cv2.imshow('Real Time', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
